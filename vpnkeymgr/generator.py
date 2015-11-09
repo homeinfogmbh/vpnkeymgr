9fae45c68ebba210919ffee974d9584ef03e18f4
Richard Neumann <r.neumann@homeinfo.de>
2015-11-09 12:06:51 +0100

    Implemented key packager and added configuration template file

--- vpnkeymgr/generator.py
+++ vpnkeymgr/generator.py
@@ -1,6 +1,8 @@
 """OpenVPN key generator"""
 
 from uuid import uuid4
+from subprocess import run, CalledProcessError
+from os.path import dirname, basename
 
 
 class Keygen():
@@ -13,9 +15,27 @@ class Keygen():
     @property
     def vars_file(self):
         """Returns the easy-rsa vars file"""
-        return self._ca
+        return self._vars_file
 
-    def generate(self, name=None):
+    def genkey(self, name):
         """Generates a new key"""
-        name = str(uuid4()) if name is None else name
-        # TODO: implement
+        basedir = dirname(self.vars_file)
+        vars_ = basename(self.vars_file)
+        cmd = 'cd {basedir}; source {vars}; build-key --batch {name}'.format(
+            basedir=basedir, vars=vars_, name=name)
+        cp = run(cmd)
+        try:
+            cp.check_returncode()
+        except CalledProcessError:
+            return False
+        else:
+            return True
+
+    def genkeys(self, count):
+        """Generates multiple keys"""
+        result = True
+        for _ in range(0, count):
+            name = str(uuid4())
+            reply = self.genkey(name)
+            result = reply and result
+        return result
--- vpnkeymgr/packer.py
+++ vpnkeymgr/packer.py
@@ -1,6 +1,6 @@
 """OpenVPN key packager"""
 
-from tempfile import TemporaryFile
+from tempfile import TemporaryFile, NamedTemporaryFile
 from os import getcwd
 from os.path import join
 
@@ -13,6 +13,10 @@ class ClientKeyPackager():
     KEYFILE = '{0}.key'
     CRTFILE = '{0}.crt'
     CA_FILE = 'ca.crt'
+    CONFIG_FILE = '{0}.conf'
+    CFG_TEMP = '/usr/share/terminals/openvpn.conf.temp'
+    AUTH = 'SHA-512'
+    CIPHER = 'CAMELLIA-256-CBC'
 
     def __init__(self, basedir=None):
         """Sets the base directory"""
@@ -23,10 +27,22 @@ class ClientKeyPackager():
         keyfile = self.KEYFILE.format(client)
         crtfile = self.CRTFILE.format(client)
         ca_file = self.CA_FILE
+        config_file = self.CONFIG_FILE.format(client)
+        # Read configuration template
+        with open(self.CFG_TEMP, 'r') as cfg_tempf:
+            cfg_temp = cfg_tempf.read()
+        # Render configuration template
+        config = cfg_temp.fomat(
+            key=keyfile, crt=crtfile, ca=ca_file,
+            auth=self.AUTH, cipher=self.CIPHER)
+        # Add files to temporary archive
         with TemporaryFile(mode='w+b') as tmp:
             with tarfile.open(mode='w', fileobj=tmp) as tar:
                 tar.add(join(self._basedir, keyfile), arcname=keyfile)
                 tar.add(join(self._basedir, crtfile), arcname=crtfile)
                 tar.add(join(self._basedir, ca_file), arcname=ca_file)
+                with NamedTemporaryFile(mode='w+') as cfg:
+                    cfg.write(config)
+                    tar.add(cfg.name, arcname=config_file)
             tmp.seek(0)
             return tmp.read()
