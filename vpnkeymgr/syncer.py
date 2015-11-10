3e6fc5042a3aba5919f102fab7b6d9f69ecb2830
Richard Neumann <r.neumann@homeinfo.de>
2015-11-10 11:16:36 +0100

    fix

--- vpnkeymgr/syncer.py
+++ vpnkeymgr/syncer.py
@@ -21,7 +21,8 @@ class Syncer():
            '-o UserKnownHostsFile=/dev/null '
            '-o StrictHostKeyChecking=no '
            '-o ConnectTimeout=5" '
-           '{arcname} {user}@{host}:{fname}')
+           '--chmod=F640 '
+           '{files} {user}@{host}:{path}')
 
     def __init__(self, basedir, *clients):
         """Sets desired clients and an optional basedir"""
@@ -36,23 +37,24 @@ class Syncer():
         path = path or self.PATH
         user = user or self.USER
         identity = identity or self.IDENTITY
+        files = []
         failures = []
         with TemporaryDirectory() as tmpdir:
             for client in self._clients:
                 tarfile = '{0}.tar'.format(client)
-                fname = join(path, tarfile)
                 arcname = join(tmpdir, tarfile)
+                files.append(arcname)
                 packager = ClientPackager(self._basedir)
                 with open(arcname, 'wb') as tar:
                     tar.write(packager(client))
-                cmd = self.CMD.format(
-                    identity=identity, arcname=arcname,
-                    user=user, host=host, fname=fname)
-                completed_process = run(cmd, shell=True)
-                try:
-                    completed_process.check_returncode()
-                except CalledProcessError:
-                    failures.append(client)
+            cmd = self.CMD.format(
+                identity=identity, files=' '.join(files),
+                user=user, host=host, path=path)
+            completed_process = run(cmd, shell=True)
+            try:
+                completed_process.check_returncode()
+            except CalledProcessError:
+                failures.append(client)
         if failures:
             print('Could not synchronize:', failures, file=stderr)
             return False
