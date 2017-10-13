34cd3c68898ee452fcbffe934c7604bb84909e4a
Richard Neumann <r.neumann@homeinfo.de>
2017-10-13 12:14:54 +0200

    Refac.

--- vpnkeymgr/__init__.py
+++ vpnkeymgr/__init__.py
@@ -1,1 +1,7 @@
-"""OpenVPN key management library"""
+"""OpenVPN key management library."""
+
+from .common import print_called_process_error
+from .generator import Keygen
+from .syncer import Syncer
+
+__all__ = ['print_called_process_error', 'Keygen', 'Syncer']
