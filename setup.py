cbd39d40c5141327de28a8a69b1749b7abaf0c04
Richard Neumann <mail@richard-neumann.de>
2021-03-27 23:58:11 +0100

    Migrated to setuptools

--- setup.py
+++ setup.py
@@ -1,15 +1,22 @@
 #! /usr/bin/env python3
 
-from distutils.core import setup
+from setuptools import setup
 
 setup(
     name='vpnkeymgr',
-    version='latest',
+    use_scm_version={
+        "local_scheme": "node-and-timestamp"
+    },
+    setup_requires=['setuptools_scm'],
     author='HOMEINFO - Digitale Informationssysteme GmbH',
-    author_email='<info at homeinfo dot de>',
+    author_email='<info@homeinfo.de>',
     maintainer='Richard Neumann',
-    maintainer_email='<r dot neumann at homeinfo priod de>',
-    requires=['docopt'],
+    maintainer_email='<r.neumann@homeinfo.de>',
     packages=['vpnkeymgr'],
-    scripts=['files/vpnkeymgr'],
-    description=('OpenVPN key manager for easyrsa3.'))
+    entry_points={
+        'console_scripts': [
+            'vpnkeymgr = vpnkeymgr.cli:main'
+        ]
+    },
+    description='OpenVPN key manager for easyrsa3.'
+)
